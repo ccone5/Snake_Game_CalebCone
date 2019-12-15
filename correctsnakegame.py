568bc2fcc6fe990b1ffe02210bb3bc0c6500fb88
KhalibGarrett <54278059+KhalibGarrett@users.noreply.github.com>
2019-12-14 19:57:23 -0500

    Update correctsnakegame.py

--- correctsnakegame.py
+++ correctsnakegame.py
@@ -1,5 +1,13 @@
+import pygame
+import time
+import random
 import pygame as pg
 import sys,random
+pygame.init()
+
+crash_sound = pygame.mixer.Sound("crash.wav")
+pygame.mixer.music.load('jazz.wav')
+pygame.mixer.music.play(-1)
 def collide(x1,y1,x2,y2,x3,y3,x4,y4):
     if (x3+x4) > x1 > x3 and (y3+y4) > y1 > y3 or (x3+x4) > x2 >x3 and (y3+y4) > y2 > y3:
         return True
@@ -342,3 +350,4 @@ def menu():
     m = startmenu()
     m.mainloop()
 menu()
+
